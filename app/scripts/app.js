544108e527dbd847d1f0f643445fba29cab78881
Vidya Kusuma <Vidya Kusuma>
2017-01-01 23:18:58 +0530

    ass2 changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -157,19 +157,23 @@
  .controller('DishCommentController', ['$scope', function($scope) {
 
      //Step 1: Create a JavaScript object to hold the comment from the form
-     $scope.cmts = {rating:"", comment="", author=""};
+     $scope.mycomment = {rating:"5", comment="", author=""};
 
      $scope.submitComment = function () {
 
          //Step 2: This is how you record the date
-         "The date property of your JavaScript object holding the comment" = new Date().toISOString();
+         $scope.mycomment.date= new Date().toISOString();
+         console.log($scope.mycomment);
 
          // Step 3: Push your comment into the dish's comment array
-         $scope.dish.comments.push("Your JavaScript Object holding the comment");
+         $scope.dish.comments.push($scope.mycomment);
 
          //Step 4: reset your form to pristine
+         $scope.commentForm.$setpristine();
 
          //Step 5: reset your JavaScript object that holds your comment
+
+         $scope.mycomment = {rating:"5", comment="", author=""};
      }
  }])
 
